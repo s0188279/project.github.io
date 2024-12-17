4eb9f38404bc969770549170ca7f6d25a355417d
s0188279 <anya_atamas@mail.ru>
2024-12-17 22:05:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    const menuList = document.querySelector('.menu-list');
+    menuList.classList.toggle('active');
+});
+

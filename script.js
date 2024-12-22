a470106d4ad9351d590b1dc4c55f6731a5040a81
s0188279 <anya_atamas@mail.ru>
2024-12-22 22:39:30 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.menu-toggle').addEventListener('click', function () {
-    const menuList = document.querySelector('.menu-list');
-    menuList.classList.toggle('active');
-});
-
